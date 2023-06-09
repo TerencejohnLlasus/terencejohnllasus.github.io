6c01e1fdcc8db7a878a33a253daa623d28e57644
TerencejohnLlasus <135990122+TerencejohnLlasus@users.noreply.github.com>
2023-06-09 10:23:32 +0800

    Add files via upload

---
+++ MyWebsite/home.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Add smooth scrolling effect to navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
